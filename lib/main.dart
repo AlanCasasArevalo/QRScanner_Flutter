0e3d8b1af2f6fa95eecfed9d09c99baf2c5a4e72
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-15 20:04:51 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:qrscanner/pages/MapPage.dart';
+import 'package:qrscanner/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QRScanner',
+      initialRoute: 'home_page',
+      routes: {
+        'home_page' : (BuildContext context) => HomePage(),
+        'map_page' : (BuildContext context) => MapPage(),
+      },
+    );
+  }
+}
