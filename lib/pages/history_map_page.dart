b9562df2df1d41d5c1770b8e5316401f6c2ca389
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-17 13:09:01 +0100

    [FEATURE][DELETE ALL DATABASE] * Delete element by id was implement with swipe

--- lib/pages/history_map_page.dart
+++ lib/pages/history_map_page.dart
@@ -12,12 +12,26 @@ class HistoryMapPage extends StatelessWidget {
 
     return ListView.builder(
       itemCount: scans.length,
-        itemBuilder: (_, index) => ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title: Text(scans[index].value),
-          subtitle: Text('ID: ${scans[index].id.toString() }'),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print('${scans[index].id}'),
-    ));
+        itemBuilder: (_, index) => Dismissible(
+          key: UniqueKey(),
+          background: Container(
+            color: Colors.red,
+            child: Center(child: Text('Deleting .....', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),)),
+          ),
+          onDismissed: (DismissDirection direction) {
+            // Debemos poner el listen a falso para que no nos de error al borrar los elementos Failed assertion: line 262 pos 7: 'context.owner.debugBuilding ||
+            //           listen == false ||
+            //           debugIsInInheritedProviderUpdate'
+            // Provider.of<ScanListProvider>(context).deleteElementById(scans[index].id);
+            Provider.of<ScanListProvider>(context, listen: false).deleteElementById(scans[index].id);
+          },
+          child: ListTile(
+            leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+            title: Text(scans[index].value),
+            subtitle: Text('ID: ${scans[index].id.toString() }'),
+            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+            onTap: () => print('${scans[index].id}'),
+    ),
+        ));
   }
 }
