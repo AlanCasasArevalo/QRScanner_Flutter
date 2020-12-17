011753ed98cf7357ed576f850739549bc4c2c791
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-17 12:46:15 +0100

    [FEATURE][SCAN LIST PROVIDER] * Some methods were renamed

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -5,7 +5,7 @@ class ScanListProvider extends ChangeNotifier {
   List<ScanModel> scans = [];
   String typeSelected = 'http';
 
-  newScan(String value) async {
+  addNewElement(String value) async {
     final newScan = ScanModel(value: value);
     final id = await DBProvider.db.addNewElement(newScan);
     newScan.id = id;
@@ -16,26 +16,26 @@ class ScanListProvider extends ChangeNotifier {
     }
   }
 
-  loadScans() async {
+  getAllElements() async {
     final scans = await DBProvider.db.getAllElements();
     this.scans = [...scans];
     notifyListeners();
   }
 
-  loadScansByType(String type) async {
+  getElementsByTypeRaw(String type) async {
     final scans = await DBProvider.db.getElementsByTypeRaw(type);
     this.scans = [...scans];
     this.typeSelected = type;
     notifyListeners();
   }
 
-  deleteAllScans () async {
+  deleteAllElement () async {
     await DBProvider.db.deleteAllElement();
     this.scans = [];
     notifyListeners();
   }
 
-  deleteScanById (int id) async {
+  deleteElementById (int id) async {
     await DBProvider.db.deleteElementById(id);
     this.scans.removeWhere((element) => id == element.id);
     notifyListeners();
