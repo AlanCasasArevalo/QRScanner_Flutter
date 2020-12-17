2d5a7599098caa96e54dacf6a7f57f7bf45e1786
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-17 16:12:05 +0100

    [FEATURE][MAP DETAIL] * Location button was added * Location to change map was added

--- lib/pages/map_detail_page.dart
+++ lib/pages/map_detail_page.dart
@@ -36,14 +36,37 @@ class _MapDetailPageState extends State<MapDetailPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_location_outlined),
+            onPressed: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(
+                  CameraUpdate.newCameraPosition(CameraPosition(
+                      target: _scanModel.getCoordinates(),
+                      zoom: 17,
+                      tilt: 50,
+                      bearing: 20
+                  ))
+              );
+            },
+          )
+        ],
       ),
       body: GoogleMap(
         myLocationEnabled: true,
+        myLocationButtonEnabled: false,
         mapType: MapType.normal,
         initialCameraPosition: _initialPoint,
         markers: markers,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: () {
+
         },
       ),
     );
