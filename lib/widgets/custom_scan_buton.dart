33a93a4346dbc1e5c8b58a8513f75cf45ef25483
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-17 13:49:38 +0100

    [FEATURE][SCAN BUTTON] * If user scan a geo will send to map detail page, if user scan a url will send it to a url scanned

--- lib/widgets/custom_scan_buton.dart
+++ lib/widgets/custom_scan_buton.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qrscanner/providers/scan_list_provider.dart';
+import 'package:qrscanner/utils/utils.dart';
 
 class CustomScanButton extends StatelessWidget {
   @override
@@ -13,8 +14,14 @@ class CustomScanButton extends StatelessWidget {
         child: Icon(Icons.filter_center_focus),
         onPressed: () async {
           final barcodeScanRes = 'https://developer.android.com/studio/debug/dev-options';
-          scanListProvider.addNewElement(barcodeScanRes);
-          scanListProvider.addNewElement('geo:15.44,23.44');
+          // final barcodeScanRes = 'geo:40.0967324,-4.0538371';
+
+          // si el barcode no es de los que hemos implementado se pasa del tema.
+          if(barcodeScanRes == '-1') {
+            return;
+          }
+          final scan = await scanListProvider.addNewElement(barcodeScanRes);
+          launchURL(context, scan);
         });
   }
 }
